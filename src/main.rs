7a0893fec4ff3c980c0fba2a3a00290d8e191c8f
gitavk <3apa3ka3@gmail.com>
2024-03-28 13:56:28 +0100

    Greet end point.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use axum::extract::Path;
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(greet))
+        .route("/:name", get(greet));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn greet(
+    Path(params): Path<HashMap<String, String>>,
+    ) -> String {
+    let default = "World".to_string();
+    let name = params.get("name").unwrap_or(&default);
+    format!("hello {}", name)
 }
