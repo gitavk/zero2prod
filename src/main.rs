98d8db0ba53bfeb4891133e4eebd20a637ed7b4d
gitavk <3apa3ka3@gmail.com>
2024-03-30 13:01:58 +0100

    CH-3.3.3: health_check end point.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use axum::{
     routing::get,
     Router,
+    http::StatusCode,
 };
-use axum::extract::Path;
-use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() {
@@ -12,18 +11,13 @@ async fn main() {
 
     // build our application with a route
     let app = Router::new()
-        .route("/", get(greet))
-        .route("/:name", get(greet));
+        .route("/health_check", get(health_check));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn greet(
-    Path(params): Path<HashMap<String, String>>,
-    ) -> String {
-    let default = "World".to_string();
-    let name = params.get("name").unwrap_or(&default);
-    format!("hello {}", name)
+async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
