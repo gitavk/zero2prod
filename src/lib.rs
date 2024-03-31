f89f9cddca9790a905f67a46f62c7fb956c54e5a
gitavk <3apa3ka3@gmail.com>
2024-03-31 20:19:36 +0200

    CH-3.87: Subscribe form without processing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,26 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Form, Router,
+};
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+async fn subscribe(Form(form): Form<FormData>) -> String {
+    format!("Welcome {}! Will sent news to {}", form.name, form.email)
+}
 async fn health_check() -> StatusCode {
     StatusCode::OK
 }
 
 pub fn app() -> Router {
     // build our application with a route
-    Router::new().route("/health_check", get(health_check))
+    Router::new()
+        .route("/health_check", get(health_check))
+        .route("/subscriptions", post(subscribe))
 }
 
 pub async fn run() -> () {
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,9 @@
-use axum::{body::Body, http::Request};
-use http_body_util::BodyExt; // for `collect`
+use axum::{
+    body::Body,
+    http::{self, Request, StatusCode},
+};
+use http_body_util::BodyExt;
+// for `collect`
 use tower::ServiceExt; // for `oneshot`
 
 // `tokio::test` is the testing equivalent of `tokio::main`.
@@ -11,6 +15,7 @@ use tower::ServiceExt; // for `oneshot`
 async fn health_check_works() {
     // Arrange
     let app = zero2prod::app();
+    // Act
     let response = app
         .oneshot(
             Request::builder()
@@ -24,4 +29,64 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     let body = response.into_body().collect().await.unwrap().to_bytes();
     assert!(body.is_empty());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app = zero2prod::app();
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/subscriptions")
+                .header(
+                    http::header::CONTENT_TYPE,
+                    "application/x-www-form-urlencoded",
+                )
+                .body(Body::from(body))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+    assert_eq!(response.status(), StatusCode::OK);
+    println!("Test form {:?}", response);
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_422_when_data_is_missing() {
+    // Arrange
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    // Act
+    for (invalid_body, error_message) in test_cases {
+        let app = zero2prod::app();
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .method(http::Method::POST)
+                    .uri("/subscriptions")
+                    .header(
+                        http::header::CONTENT_TYPE,
+                        "application/x-www-form-urlencoded",
+                    )
+                    .body(Body::from(invalid_body))
+                    .unwrap(),
+            )
+            .await
+            .unwrap();
+        // Assert
+        println!("Test form {:?}", response);
+        assert_eq!(
+            422,
+            response.status().as_u16(),
+            "The API did not fail with 422 Bad Request when payload was {}.",
+            error_message,
+        );
+    }
 }
